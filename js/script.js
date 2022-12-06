8e1e4bd86d7c8e8e4fcc88e9ba71f00b933d036e
Alessandro-Villani <alessvill@libero.it>
2022-12-06 15:28:18 +0100

    defined basic outline

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 /*------------------------------------TRACCIA-------------------------------------------
+
 Scrivere un programma che chieda all’utente:
 - Il numero di chilometri da percorrere
 - Età del passeggero
@@ -11,4 +12,18 @@ Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente
 MILESTONE 2:
 Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
 ----------------------------------------------------------------------------------------*/
+
+/*--------------------------------------TODO----------------------------------------------
+
+1. Store target in variable
+2. Set price/km in a variable
+3. Set discout value variables
+4. Add listener for submit button
+    4.1 Validate datas
+    4.2 Set the outcome depending on the inserted datas
+5. Print price on page.
+
+----------------------------------------------------------------------------------------*/
+
